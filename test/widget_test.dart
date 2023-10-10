2597c046927828fe0e037fafbb292c011b11aae0
Finn Dilan <finn.dilan@gmail.com>
2023-10-10 10:59:19 +0200

    Removed the hrw

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:hrw_studiconnect/main.dart';
+import 'package:studiconnect/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
