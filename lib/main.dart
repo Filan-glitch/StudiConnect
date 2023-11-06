e89dd84615884bffde6d2edd839c196b350c6fd6
Finn Dilan <finn.dilan@gmail.com>
2023-11-06 11:00:59 +0100

    main fixed and loginpage improved

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,14 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_redux/flutter_redux.dart';
 
 import 'firebase_options.dart';
-import 'model/redux/app_state.dart';
-import 'model/redux/store.dart';
 import 'pages/login_page.dart';
-import 'themes/dark_theme.dart';
-import 'themes/light_theme.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-
   runApp(const MyApp());
 }
 
@@ -22,16 +17,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return StoreProvider<AppState>(
-      store: store,
-      child: MaterialApp(
-        title: 'StudiConnect',
-        theme: lightTheme,
-        darkTheme: darkTheme,
-        themeMode: ThemeMode.system,
-        supportedLocales: const [Locale('en', 'US'), Locale('de', 'DE')],
-        home: const LoginPage(),
-      ),
-    );
+    return const LoginPage();
   }
 }
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,22 +1,49 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
-class LoginPage extends StatefulWidget {
+class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
 
   @override
-  State<LoginPage> createState() => _LoginPageState();
-}
-
-class _LoginPageState extends State<LoginPage> {
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Login Page'),
-      ),
-      body: const Center(
-        child: Text('This is the login page'),
-      ),
-    );
+        body: Center(
+      child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text('StudiConnect'),
+            SizedBox(
+              width: MediaQuery.of(context).size.width * 0.80,
+              child: const Text(
+                  textAlign: TextAlign.center,
+                  'Mit deiner Anmeldung stimmst du unseren AGBs zu. In unserer Datenschutzerklärung findest du weitere Informationen zur Verarbeitung deiner Daten.'),
+            ),
+            ElevatedButton.icon(
+                onPressed: () {
+                  Navigator.pushNamed(context, '/mail-login');
+                },
+                label: const Text('Mit E-Mail anmelden'),
+                icon: const Icon(Icons.login)),
+            ElevatedButton.icon(
+              onPressed: () {
+                Navigator.pushNamed(context, '/google-login');
+              },
+              label: const Text('Mit Google anmelden'),
+              icon: SvgPicture.asset('assets/icons/google_logo.svg',
+                  semanticsLabel: 'Google Logo'),
+            ),
+            RichText(
+              text: TextSpan(
+                text: 'Probleme bei der Anmeldung?',
+                style: DefaultTextStyle.of(context).style,
+                recognizer: TapGestureRecognizer()
+                  ..onTap = () {
+                    Navigator.pushNamed(context, '/login-help');
+                  },
+              ),
+            )
+          ]),
+    ));
   }
 }
