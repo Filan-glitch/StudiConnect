2a15db22b95ceaaacb307ed9bd1ce2d54b67fe45
Finn Dilan <finn.dilan@gmail.com>
2023-11-06 11:35:52 +0100

    pages created and redux deleted

--- lib/model/redux/store.dart
+++
@@ -1,9 +0,0 @@
-import 'package:redux/redux.dart';
-
-import 'app_state.dart';
-import 'reducer.dart';
-
-final Store<AppState> store = Store<AppState>(
-  appReducer,
-  initialState: AppState(),
-);
