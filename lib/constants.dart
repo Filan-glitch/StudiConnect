177c3151a8d830bfd571ecad222c91395f59e570
Jan Bellenberg <janbellenberg.jb@gmail.com>
2024-01-09 17:12:17 +0100

    Updated backend url

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,12 +1,5 @@
-const backendURL = /*kDebugMode
-    ? 'http://192.168.178.21:8080'
-    : */
-    'https://studiconnect.janbellenberg.de';
-
-const wsURL = /*kDebugMode
-    ? 'ws://192.168.178.21:8081'
-    : */
-    'wss://studiconnect.janbellenberg.de';
+const backendURL = 'https://studiconnect.janbellenberg.de';
+const wsURL = 'wss://studiconnect.janbellenberg.de';
 
 const termsURL = "https://studiconnect.janbellenberg.de/terms";
 const imprintURL = "https://studiconnect.janbellenberg.de/imprint";
