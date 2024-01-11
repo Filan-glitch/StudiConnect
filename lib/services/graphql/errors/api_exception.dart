396bd2e55457ee18c60c7659bf9e0da072886704
Finn Dilan <finn.dilan@gmail.com>
2024-01-11 18:43:44 +0100

    fixed merge artifacts

--- lib/services/graphql/errors/api_exception.dart
+++ lib/services/graphql/errors/api_exception.dart
@@ -1,10 +1,31 @@
+/// This library contains the [ApiException] class.
+///
+/// {@category EXCEPTIONS}
+library services.error.api_exception;
+
+/// Represents an API exception.
+///
+/// This class implements the [Exception] interface and provides a custom
+/// exception type for handling API errors. It includes an optional error
+/// message and an error code.
+///
+/// The [message] parameter is optional and defaults to a generic error message.
+/// It represents the error message associated with the exception.
+///
+/// The [code] parameter is optional and represents the error code associated
+/// with the exception.
 class ApiException implements Exception {
   final String message;
   final int? code;
 
+  /// The default error message to be used if no custom message is provided.
   static const String defaultMessage =
       'Es ist ein unbekannter Fehler aufgetreten.';
 
+  /// Creates an [ApiException].
+  ///
+  /// The [message] parameter defaults to [defaultMessage] if not provided.
+  /// The [code] parameter is optional.
   ApiException({
     this.message = defaultMessage,
     this.code,
--- lib/services/graphql/errors/not_found_exception.dart
+++ lib/services/graphql/errors/not_found_exception.dart
@@ -1,11 +1,29 @@
+/// This library contains the [NotFoundException] class.
+///
+/// {@category EXCEPTIONS}
+library services.error.not_found_exception;
+
 import 'package:studiconnect/services/graphql/errors/api_exception.dart';
 
+/// Represents a not found exception.
+///
+/// This class extends the [ApiException] class and provides a custom
+/// exception type for handling not found errors. It includes a default error
+/// message and a default error code.
+///
+/// The [message] parameter is optional and defaults to a not found error message.
+/// It represents the error message associated with the exception.
+///
+/// The [code] parameter is optional and defaults to 404. It represents the error code associated
+/// with the exception.
 class NotFoundException extends ApiException {
   NotFoundException({
     super.message = defaultMessage,
     super.code = 404,
   });
 
+  /// The default error message to be used if no custom message is provided.
+  /// It indicates that the requested resource was not found.
   static const String defaultMessage =
       'Die angeforderte Ressource wurde nicht gefunden.';
 }
